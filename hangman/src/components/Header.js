b9fb30f8110bb398711166665f36f625a4d647b7
carmenlee96 <104672812+carmenlee96@users.noreply.github.com>
2022-05-29 22:07:34 +0800

    Update Header.js

--- hangman/src/components/Header.js
+++ hangman/src/components/Header.js
@@ -4,8 +4,8 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-      <h1>Hangman</h1>
-      <p>Enter a letter</p>
+      <h1>Lets Play Hangman</h1>
+      <p>Enter the correct letter to win before your man dies</p>
     </>
   )
 }
